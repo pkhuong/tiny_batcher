6f20a1f5f4a4f64dd71aee3e390da5a205c6972d
Paul Khuong <pvk@pvk.ca>
2025-08-24 11:05:30 -0400

    test.sh: exercise larger sizes
    Compare a prefix (long enough to cover 2**20) of the traces.

--- test.sh
+++ test.sh
@@ -9,7 +9,11 @@ for idx in `seq 0 1200`; do
     diff -q <(./test $idx) <(python3 reference.py $idx) >/dev/null || echo "Failed $idx";
 done
 
-for idx in `seq 20`; do
-    diff -q <(./test $((1 << $idx))) <(python3 reference.py $((1 << $idx))) >/dev/null || \
+# enough to fully sort up to 2**20
+LIMIT=$((10 * 10 * 1024 * 1024))
+
+for idx in `seq 60`; do
+    diff -q <(./test $((1 << $idx)) | head -n $LIMIT) \
+            <(python3 reference.py $((1 << $idx)) 2>/dev/null | head -n $LIMIT) >/dev/null || \
         echo "Failed $((1 << $idx))";
 done
