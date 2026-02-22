f609c2c3be302fc234482c8644e19751ad27e3e5
Paul Khuong <pvk@pvk.ca>
2026-02-22 12:35:28 -0500

    reference.py: clarify reference for the swap index generator

--- reference.py
+++ reference.py
@@ -3,6 +3,9 @@ import sys
 # https://oeis.org/A375649: Number of comparisons and swaps in the
 # Batcher odd-even merge sort needed to sort n items, plus an extra
 # 0 for n = 0.
+#
+# We actually implement a slightly different algorithm (still by
+# Batcher) that needs slightly fewer swaps.
 EXPECTED_SWAPS = [
     0, 0, 1, 3, 5, 9, 12, 16, 19, 28, 32, 38, 42, 48, 53, 59, 63, 85,
     90, 98, 103, 112, 119, 127, 132, 140, 147, 156, 162, 171, 178,
@@ -12,8 +15,8 @@ EXPECTED_SWAPS = [
 ]
 
 
-# A generator of indices for Batcher's odd/even merge sort,
-# transliterated from TAoCP Vol 3, 2nd edition.
+# A generator of indices for Batcher's merge-exchange sort transliterated
+# from TAoCP Vol 3, 2nd edition (Section 5.2.2, p111).
 def algorithM(N):
     if N < 2:  # Assume N >= 2
         return
